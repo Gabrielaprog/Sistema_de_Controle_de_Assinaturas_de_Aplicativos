0a944d2c6cf1c13ea3e019c5b30bf84dc32d271a
Gabriela Silveira <137567010+Gabrielaprog@users.noreply.github.com>
2025-06-05 18:01:30 -0300

    Delete Gabriela Silveira Trindade Rodrigues - desenvolvimento backend - fase 2 directory

--- Gabriela Silveira Trindade Rodrigues - desenvolvimento backend - fase 2/src/entities/index.ts
+++
@@ -1,6 +0,0 @@
-import { Aplicativo } from './aplicativo.entity';
-import { Assinatura } from './assinatura.entity';
-import { Pagamento } from './pagamento.entity';
-import { Cliente } from './client.entity';
-
-export { Aplicativo, Assinatura, Pagamento, Cliente };
