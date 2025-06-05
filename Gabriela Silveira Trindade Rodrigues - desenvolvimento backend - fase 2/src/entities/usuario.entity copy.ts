cd58b0d0c70d3167b2ba1db2193c5a7ae771ed07
Gabriela <gabrielasilveira309@gmail.com>
2025-06-05 17:26:07 -0300

    primeiro commit

---
+++ Gabriela Silveira Trindade Rodrigues - desenvolvimento backend - fase 2/src/entities/usuario.entity copy.ts
@@ -0,0 +1,11 @@
+// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// @Entity()
+// export class Usuario {
+//   @Column()
+//   usuario: string;
+
+//   @Column('float')
+//   senha: number;
+// }
+// Comentado pois não existe chave primária na documentação.
